ac94fdb48c2beed5553db8c26ca2fcc232dd434b
bahadiri <ayd@Bahadirs-MacBook-Pro.local>
2023-03-11 13:23:07 -0800

    Incremental changes

--- main.py
+++ main.py
@@ -25,3 +25,8 @@ def search(query: fixieai.Message) -> str:
     # TODO: find and return a youtube video
     assert query.text == "input"
     return "output"
+
+    text = query.text.replace("[[", "[").replace("]]", "]")
+    url = "https://www.youtube.com/results?search_query=" + urllib.parse.quote(text)
+    embed = fixieai.Embed(content_type="image/png", uri=url)
+    return fixieai.Message(text="Here you go! [image1]", embeds={"image1": embed})
