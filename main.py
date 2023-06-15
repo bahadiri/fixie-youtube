1ccf919fb6dad4c56e2fbcf03fdf95d4af303548
bia-poly <bahadir.aydin@polysign.io>
2023-06-15 05:56:02 -0700

    Bank statement agent is initiated

--- main.py
+++ main.py
@@ -1,76 +1,37 @@
-import urllib.parse
-from bs4 import BeautifulSoup
-import re
-import random
+"""This agent allows the user to extract end balance from a statement provided as a URL.
+"""
 
 import fixieai
+import requests
 
-BASE_PROMPT = """I am an agent that finds and recommends Youtube videos."""
+BASE_PROMPT = """I am a simple agent that allows the user to ask questions about a web page.
+I ask Func[load_doc] to load the resource from a URL, and then always reach out to
+Func[fixie_query_embed] to answer questions about the resource."""
 
 FEW_SHOTS = """
-Q: Show me a video similar to https://www.youtube.com/watch?v=Kd_rkJObFVY
-Thought: I can recommend Youtube videos about cats that you may find interesting.
-Ask Func[search]: cats
-Func[search] says: https://www.youtube.com/watch?v=WeiONy5Q9cQ [image1]
-A: You can watch https://www.youtube.com/watch?v=WeiONy5Q9cQ [image1]
+Q: What is the end balance in: http://some_url
+Ask Func[load_doc]: http://some_url
+Func[load_doc] says: #doc1
+Ask Func[fixie_query_embed]: What is the end balance in this document #doc1
+Func[fixie_query_embed] says: The end balance found in the resource
+A: The end balance found in the resource
 
-Q: Show me a video with cats?
-Thought: I can recommend Youtube videos about cats that you may find interesting.
-Ask Func[search]: cats
-Func[search] says: https://www.youtube.com/watch?v=WeiONy5Q9cQ [image1]
-A: You can watch https://www.youtube.com/watch?v=WeiONy5Q9cQ [image1]
-
-Q: Recommend me a video on dogs
-Thought: I can recommend Youtube videos about cots that you may find interesting.
-Ask Func[search]: dogs
-Func[search] says: https://www.youtube.com/watch?v=59rADhtMzWc [image1]
-A: You can watch https://www.youtube.com/watch?v=59rADhtMzWc [image1]
-
-Q: Recommend me a query based on the video.
-Ask Func[createQuery]: https://www.youtube.com/watch?v=59rADhtMzWc
-Func[createQuery] says: dogs and cows
-A: dogs and cows
-
-Q: Suggest me a video based on my calendar
-Thought: I need to get the events from now onwards..
-Ask Agent[fixie/gcalendar]: what is on my calendar
-Agent[fixie/gcalendar] says: You have a HOA meeting on Sunday, a File taxes & Finance discussion on Monday.
-Thought: Now I need to find a video about HOA meeting and File taxes & Finance discussion.
-Ask Func[search]: HOA meeting and File taxes & Finance discussion
-Func[search] says: https://www.youtube.com/watch?v=qDgsD0vlBNU [image1]
-A: https://www.youtube.com/watch?v=qDgsD0vlBNU [image1]
+Q: I want to ask questions about this statement: http://some_url
+Ask Func[load_doc]: http://some_url
+Func[load_doc] says: #doc1
+A: I've finished reading the resource. Go ahead and ask me about it.
+Q: What is the end balance in it?
+Ask Func[fixie_query_embed]: What is the end balance in #doc1
+Func[fixie_query_embed] says: answer from the resource
+A: answer from the resource
 """
-agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS)
+agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS, conversational=True)
 
 @agent.register_func
-def search(query: fixieai.Message) -> fixieai.Message:
-    # TODO: find and return a youtube video
-    text = query.text.replace("[[", "[").replace("]]", "]")
-    search_uri = "https://www.youtube.com/results?search_query=" + urllib.parse.quote(text)
-    
-    html = urllib.request.urlopen(search_uri).read().decode("utf-8") 
-    def write_to_file(text):
-        with open("youtube_results.out", "w") as f:
-            f.write(text)
-    write_to_file(html)
-    def parse_results(html):
-        regex = re.compile(r"videoId\":\"([^\"]*)")
-        videoIds = regex.findall(html)
-        
-        # regex based on "title":{"runs":[{"text":"
-        regex = re.compile(r"title\":{\"runs\":\[{\"text\":\"([^\"]*)")
-        titles = regex.findall(html)
-        
-        return list(zip(titles, videoIds))
-
-        
-    results = parse_results(html)
-    random.shuffle(results)
-    title, videoId = results[0]
-    title = "this video"
-    # print(title, videoId)
-    # videoId = results[0]
-    thumbnail_uri = "https://i.ytimg.com/vi/" + videoId + "/hq720.jpg"
-    embed = fixieai.Embed(content_type="image/jpg", uri=thumbnail_uri)
-    url = "https://www.youtube.com/watch?v=" + videoId
-    return fixieai.Message(text="You can watch " + title + " at " + url + " [image1]", embeds={"image1": embed})
+def load_doc(query: fixieai.agents.Message) -> fixieai.Message:
+    url = query.text
+    response = requests.get(url)
+    content_type = response.headers["Content-Type"]
+    result =  fixieai.Message("#doc1", embeds={"doc1": fixieai.Embed(content_type, url)})
+    # print(result.embeds["doc1"].content)
+    return result
